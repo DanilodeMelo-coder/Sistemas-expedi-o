2825623f9345d2364f295e8fd91646d44a9c7737
unknown <danilo.demelo.0207@gmail.com>
2026-07-29 15:01:38 -0300

    Feat: add relationship na tabela motoboys

--- app/models/motoboy_model.py
+++ app/models/motoboy_model.py
@@ -1,13 +1,16 @@
 from core.database import Base
 from sqlalchemy import String, Boolean, Integer, Column
+from sqlalchemy.orm import relationship
 import uuid
 
 
 class Motoboy(Base):
-    __tablename__ = "Motoboys"
+    __tablename__ = "motoboys"
 
 
     id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()))
     nome = Column(String, nullable=False)
-    ativo = Column(Boolean, nullable=False)
-    disponivel = Column(Boolean, nullable=False)
+    ativo = Column(Boolean, nullable=True)
+    disponivel = Column(Boolean, nullable=True)
+
+    saidas = relationship("Saida", back_populates="motoboy")

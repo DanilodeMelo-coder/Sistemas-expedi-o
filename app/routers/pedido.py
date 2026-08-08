79f845665fa534182e8e396afcd807b7d515eb10
DanilodeMelo-coder <danilo.demelo.0207@gmail.com>
2026-08-07 22:50:00 -0300

    Feat: Imprementando services e rotas de cancelamento e atualização de pedidos

--- app/routers/pedido.py
+++ app/routers/pedido.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from app.services.pedidos_service import criar_pedido_service, listar_pedidos_service
+from app.services.pedidos_service import atualizar_pedido_service, cancelar_pedido_service, criar_pedido_service, listar_pedidos_service
 from app.schemas.schema_pedido import PedidoSchema_Request, PedidoSchema_Response
 from core.database import get_session
 
@@ -18,3 +18,11 @@ def criar_pedido(dados: PedidoSchema_Request, db: Session = Depends(get_session)
 @router.get("/", response_model=list[PedidoSchema_Response])
 def listar_pedidos(db: Session = Depends(get_session)):
     return listar_pedidos_service(db)
+
+@router.put("/{pedido_id}", response_model=PedidoSchema_Response)
+def atualizar_pedido(pedido_id: str, dados: PedidoSchema_Request, db: Session = Depends(get_session)):
+    return atualizar_pedido_service(db, pedido_id, dados)
+
+@router.get("/{pedido_id}/cancelar", response_model=PedidoSchema_Response)
+def cancelar_pedido(pedido_id: str, db: Session = Depends(get_session)):
+    return cancelar_pedido_service(db, pedido_id)
--- app/services/pedidos_service.py
+++ app/services/pedidos_service.py
@@ -1,3 +1,6 @@
+from fastapi import HTTPException
+
+from app.models.enum_status_pedido import Status_PedidoE
 from app.schemas.schema_pedido import PedidoSchema_Request
 from app.models.pedido_model import Pedido
 from sqlalchemy.orm import Session 
@@ -27,7 +30,44 @@ def criar_pedido_service(db: Session, dados: PedidoSchema_Request):
     return pedido_novo
 
 
-#listar motoboys
+#listar pedidos
 def listar_pedidos_service(db: Session):
     pedidos = db.query(Pedido).all()
     return pedidos
+
+
+
+#atualizar pedido
+def atualizar_pedido_service(db: Session, pedido_id: str, dados: PedidoSchema_Request):
+    pedido = db.query(Pedido).filter(Pedido.id == pedido_id).first()
+    if not pedido:
+        raise HTTPException(status_code=404, detail="Pedido não encontrado")
+
+    pedido.cliente = dados.cliente
+    pedido.endereco = dados.endereco
+    pedido.numero = dados.numero
+    pedido.complemento = dados.complemento
+    pedido.bairro = dados.bairro
+    pedido.cidade = dados.cidade
+    pedido.cep = dados.cep
+    pedido.horario_limite = dados.horario_limite
+    pedido.forma_pagamento = dados.forma_pagamento
+    pedido.observacao = dados.observacao
+    pedido.valor_total = dados.valor_total
+
+    db.commit()
+    db.refresh(pedido)
+
+    return pedido
+
+
+def cancelar_pedido_service(db: Session, pedido_id: str):
+    pedido = db.query(Pedido).filter(Pedido.id == pedido_id).first()
+    if not pedido:
+        raise HTTPException(status_code=404, detail="Pedido não encontrado")
+
+    pedido.status = Status_PedidoE.CANCELADO
+    db.commit()
+    db.refresh(pedido)
+
+    return pedido
